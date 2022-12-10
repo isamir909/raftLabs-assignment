83312353b59cb25cbce7cb723d61bae80e87d0b7
Samir Lohiya <104209132+isamir909@users.noreply.github.com>
2022-12-10 20:45:38 +0530

    added knightOptions.js

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
 import "./Chessboard.css"
-
+import validPosition from "./KnightOptions"
 const verticalAxis=["1","2","3","4","5","6","7","8"];
 // const horizontalAxis=["a","b","c","d","e","f","g","h"];
 const horizontalAxis=["1","2","3","4","5","6","7","8"];
-
-// interface Props{
-//   e:MouseEventHandler<HTMLDivElement>
-//   target:Array;
-// }
-
 
 export default function Chessboard(){ 
   const [selected,setSelected]=useState([])
 
   const handleClick = (event) => {
       
-    console.log(event.target.value);
-    setSelected([event.target.value])
+    // console.log(event.target.value);
+    
+    setSelected(validPosition(event.target.value))
 
   };
 
@@ -25,16 +20,15 @@ export default function Chessboard(){
     for(let i=verticalAxis.length-1;i>=0;i--){
       for(let j=0;j<horizontalAxis.length;j++){
         let num=`${horizontalAxis[j]}${verticalAxis[i]}`
-        // let num1=`${horizontalAxis[j]}${verticalAxis[i]}`
         if((i+j+2)%2===0){
           board.push(
-            <button key={`${i},${j}`}  value={num}   onClick={(event)=>handleClick(event)} className={`board-green-box ${selected.includes(horizontalAxis[j]+verticalAxis[i]) ? 'possibleWays': ''}`}> [{horizontalAxis[j]}{verticalAxis[i]}]</button>
+            <button key={`${i},${j}`}  value={num}   onClick={(event)=>handleClick(event)} className={`board-black-box ${selected.includes(horizontalAxis[j]+verticalAxis[i]) ? 'possibleWays': ''}`}> [{horizontalAxis[j]}{verticalAxis[i]}]</button>
           )
         }
         else{
           board.push(
           
-            <button  key={`${i},${j}`}  value={num}   onClick={(event)=>handleClick(event)} className="board-white-box">[{horizontalAxis[j]}{verticalAxis[i]}]</button>
+            <button  key={`${i},${j}`}  value={num}   onClick={(event)=>handleClick(event)} className={`board-white-box ${selected.includes(horizontalAxis[j]+verticalAxis[i]) ? 'possibleWays': ''}`}>[{horizontalAxis[j]}{verticalAxis[i]}]</button>
           )
         }
       }
--- src/components/KnightOptions.js
+++ src/components/KnightOptions.js
@@ -1,11 +1,30 @@
-import React from 'react'
 
-const KnightOptions = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function getPossiblePosition(input,col,row){
+if(col>0 && col<=8 && row>0 && row<=8 ){
+  return(col.toString()+row.toString())
+}
 }
 
-export default KnightOptions
+const validPosition= (val)=>{
+  let colInput=Number(val[0])
+  let rowInput=Number(val[1])
+  let result=[]
+  let possible=[
+    getPossiblePosition(val,colInput+1,rowInput+2),
+    getPossiblePosition(val,colInput+2,rowInput+1),
+    getPossiblePosition(val,colInput+2,rowInput-1),
+    getPossiblePosition(val,colInput+1,rowInput-2),
+    getPossiblePosition(val,colInput-1,rowInput+2),
+    getPossiblePosition(val,colInput-2,rowInput+1),
+    getPossiblePosition(val,colInput-2,rowInput-1),
+    getPossiblePosition(val,colInput-1,rowInput-2),
+  
+  ]
+  for(let i=0;i<possible.length;i++){
+    let temp= possible[i]
+   if(temp){result.push(temp)}
+  }
+return result
+}
+
+export default validPosition
